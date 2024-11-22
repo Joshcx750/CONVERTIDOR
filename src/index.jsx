8261d2ad3758007e72c4ff5128853d608add7cbc
unknown <ing_jgerardo@hotmail.com>
2024-11-21 19:16:40 -0600

    se agregar el proyecto

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "./styles.css";
+import "./styles.css"; // Importa el archivo CSS
 
-const App = () => {
+const ConvertXmlToPdf = () => {
   return (
     <div className="container">
       <h1 className="title">Convierte tus archivos de XML a PDF</h1>
@@ -9,18 +9,8 @@ const App = () => {
         ¿Cansado de descargar uno por uno tus archivos XML para convertirlos a PDF? Aquí te ayudamos a convertirlos todos o de uno en uno al instante.
       </p>
       <button className="button">Agregar archivos</button>
-
-      {/* Nueva sección para previsualización */}
-      <div className="preview-section">
-        <button className="preview-button">Previsualizar PDF</button>
-        <img
-          className="pdf-image"
-          src="https://via.placeholder.com/150x200.png?text=PDF"
-          alt="Vista previa de un PDF genérico"
-        />
-      </div>
     </div>
   );
 };
 
-export default App;
+export default ConvertXmlToPdf;
